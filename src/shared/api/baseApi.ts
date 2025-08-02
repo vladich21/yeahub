451da29c5c4e2f9d37248953b1035beca03e7229
Vladislav <vladkoval039@gmail.com>
2025-08-02 23:16:29 +0300

    refactoringV2

--- src/shared/api/baseApi.ts
+++ src/shared/api/baseApi.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import type { BaseQueryFn } from "@reduxjs/toolkit/query";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: import.meta.env.VITE_API_URL,
+  baseUrl: import.meta.env.VITE_BASE_URL,
 });
 
 export const baseApi = createApi({
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,25 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import path from "path";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 3000,
+//   },
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//       "@app": path.resolve(__dirname, "./src/app"),
+//       "@entities": path.resolve(__dirname, "./src/entities"),
+//       "@features": path.resolve(__dirname, "./src/features"),
+//       "@shared": path.resolve(__dirname, "./src/shared"),
+//       "@widgets": path.resolve(__dirname, "./src/widgets"),
+//       "@pages": path.resolve(__dirname, "./src/pages"),
+//     },
+//   },
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath } from "node:url";
@@ -7,6 +29,9 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
